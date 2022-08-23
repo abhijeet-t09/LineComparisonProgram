15d60ba87e4ae1436fd4b3023d1d2da1ac91158e
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-23 19:48:48 +0530

    Line Comparison Computation Program: First Commit

---
+++ src/com/bridgelabz/LineComparisonMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparisonMain {
+    public static void main(String[] args) {
+        System.out.println("Line Comparison Computation Program");
+    }
+}
