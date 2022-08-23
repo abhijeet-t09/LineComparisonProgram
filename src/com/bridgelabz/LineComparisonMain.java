61f8ddc372de2ffc8d07ce894574158d3cc943ab
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-23 20:42:38 +0530

    Length Comparison

--- src/com/bridgelabz/LineComparisonMain.java
+++ src/com/bridgelabz/LineComparisonMain.java
@@ -1,11 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class LineComparisonMain {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        LineComparisonMain objPQ=new LineComparisonMain();
+        LineComparisonMain objRS=new LineComparisonMain();
+
+        boolean lengthCheck  = Objects.equals(objPQ.lengthPQ(), objRS.lengthRS());
+
+        if (lengthCheck == true)
+            System.out.println("The lines PQ and RS are equal");
+        else
+            System.out.println("PQ and RS are not equal");
+    }
+
+
+    double lengthPQ(){
         System.out.println("Enter x1 and y1: ");
         int x1 = sc.nextInt();
         int y1 = sc.nextInt();
@@ -14,17 +30,11 @@ public class LineComparisonMain {
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
 
-        double result = lengthComputation(x1, y1, x2, y2);
-        System.out.println("Length of Line = " + result);
-    }
-
-    static double lengthComputation(int x1, int y1, int x2, int y2){
-
         double a = Math.pow(x2 - x1, 2);
         double b = Math.pow(y2 - y1, 2);
         double c = (a * x1) + (b * y1);
 
-        double length = Math.sqrt(a + b);
+        double length1 = Math.sqrt(a + b);
 
         if (b < 0) {
             System.out.println(
@@ -36,7 +46,37 @@ public class LineComparisonMain {
                     "The line passing through points P and Q is: "
                             + a + "x + " + b + "y = " + c);
         }
+        System.out.println("Length of Line = " + length1);
+        System.out.println();
+        return length1;
+    }
 
-        return length;
+    double lengthRS(){
+        System.out.println("Enter x1 and y1: ");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        System.out.println("Enter x2 and y2:");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        double a = Math.pow(x2 - x1, 2);
+        double b = Math.pow(y2 - y1, 2);
+        double c = (a * x1) + (b * y1);
+
+        double length2 = Math.sqrt(a + b);
+
+        if (b < 0) {
+            System.out.println(
+                    "The line passing through points R and S:: "
+                            + a + "x - " + b + "y = " + c);
+        }
+        else {
+            System.out.println(
+                    "The line passing through points R and S is: "
+                            + a + "x + " + b + "y = " + c);
+        }
+        System.out.println("Length of Line = " + length2);
+        return length2;
     }
 }
