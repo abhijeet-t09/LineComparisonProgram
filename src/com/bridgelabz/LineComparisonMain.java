701ca4c7810c560f089e3660ea3fe9a188a2c576
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-23 20:13:37 +0530

    Length of Line Claculation

--- src/com/bridgelabz/LineComparisonMain.java
+++ src/com/bridgelabz/LineComparisonMain.java
@@ -1,7 +1,42 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparisonMain {
     public static void main(String[] args) {
-        System.out.println("Line Comparison Computation Program");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter x1 and y1: ");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        System.out.println("Enter x2 and y2:");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        double result = lengthComputation(x1, y1, x2, y2);
+        System.out.println("Length of Line = " + result);
+    }
+
+    static double lengthComputation(int x1, int y1, int x2, int y2){
+
+        double a = Math.pow(x2 - x1, 2);
+        double b = Math.pow(y2 - y1, 2);
+        double c = (a * x1) + (b * y1);
+
+        double length = Math.sqrt(a + b);
+
+        if (b < 0) {
+            System.out.println(
+                    "The line passing through points P and Q:: "
+                            + a + "x - " + b + "y = " + c);
+        }
+        else {
+            System.out.println(
+                    "The line passing through points P and Q is: "
+                            + a + "x + " + b + "y = " + c);
+        }
+
+        return length;
     }
 }
